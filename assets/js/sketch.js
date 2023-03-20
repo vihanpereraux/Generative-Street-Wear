be2e973dcfaf31bf9dbe503dea6031ec772f3367
Vihan Perera <vihanpereraux@gmail.com>
2023-03-20 13:35:03 +0530

    Change canvas properties

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,13 +1,14 @@
-function setup() {
-  var customCanvas = createCanvas(200, 300);
-  customCanvas.parent("custom-canvas")
-  // background(255, 255, 255);
-}
+let newCanvas;
+let canvasWidth;
+let canvasHeight;
 
-function draw() {
-  
-  // fill("red")
-  // noStroke()
+function setup() {
+  canvasWidth = windowWidth/2;
+  canvasHeight = windowWidth/2
+  newCanvas = createCanvas(canvasWidth, canvasHeight);
+  newCanvas.position((windowWidth - windowWidth/2)/2, (windowHeight - windowWidth/2)/2);
+  background(0, 0, 0);
+
   noFill()
   stroke("white")
   strokeWeight(1.5)
@@ -44,4 +45,41 @@ function draw() {
 
   // vertex(0, 300);
   endShape(CLOSE);
+
+
+
+  
+  beginShape();
+  vertex(10, 0);
+  vertex(45, 0);
+  vertex(55, 10);
+
+  vertex(145, 10);
+  vertex(155, 0);
+  vertex(190, 0);
+  vertex(200, 10);
+  vertex(200, 40);
+  vertex(190, 50);
+
+  vertex(190, 250);
+  vertex(200, 260);
+  vertex(200, 290);
+  vertex(190, 300);
+  vertex(160, 300);
+  vertex(150, 290);
+
+  vertex(50, 290);
+  vertex(40, 300);
+  vertex(10, 300);
+  vertex(0, 290);
+  vertex(0, 260);
+  vertex(10, 250);
+
+  vertex(10, 50);
+  vertex(0, 40);
+  vertex(0, 10);
+
+  // vertex(0, 300);
+  endShape(CLOSE);
 }
+
